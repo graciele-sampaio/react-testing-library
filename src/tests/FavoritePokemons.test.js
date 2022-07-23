f2419d3f3689a130368a77c5a6494a1e78ced23f
Graciele <graciele@pop-os.localdomain>
2022-07-23 10:16:25 -0300

    requisito 3

--- src/tests/FavoritePokemons.test.js
+++ src/tests/FavoritePokemons.test.js
@@ -1,1 +1,21 @@
-test('', () => {});
+import React from 'react';
+import { screen } from '@testing-library/react';
+import { FavoritePokemons } from '../pages';
+import renderWithRouter from '../renderWithRouter';
+
+describe('Teste o componente <App.js />', () => {
+  test('se é exibida na tela a mensagem No favorite pokemon found', () => {
+    renderWithRouter(<FavoritePokemons />);
+
+    const messageNotFound = screen.getByText(/No favorite pokemon found/i);
+    expect(messageNotFound).toBeInTheDocument();
+  });
+
+  test('se são exibidos todos os cards de pokémons favoritados', () => {
+    renderWithRouter(<FavoritePokemons />);
+
+    const cardsFavoritesPokemons = document.querySelectorAll('.favorite-pokemon');
+    cardsFavoritesPokemons
+      .forEach((card) => expect(card).toBeInTheDocument());
+  });
+});
