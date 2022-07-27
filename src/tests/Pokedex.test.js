b18d4e95c95073d5cf958da562ceb412b940abd7
Graciele <graciele@pop-os.localdomain>
2022-07-27 13:36:31 -0300

    requisitos 5 e 6 graças a Deus

--- src/tests/Pokedex.test.js
+++ src/tests/Pokedex.test.js
@@ -1,1 +1,72 @@
-test('', () => {});
+import React from 'react';
+import { screen } from '@testing-library/react';
+import userEvent from '@testing-library/user-event';
+import renderWithRouter from '../renderWithRouter';
+import App from '../App';
+
+describe('Teste o componente <Pokedex.js />', () => {
+  test('se a página contém um heading h2 com o texto Encountered pokémons', () => {
+    renderWithRouter(<App />);
+
+    const titlePokedex = screen.getByRole('heading',
+      { name: /Encountered pokémons/i, level: 2 });
+    expect(titlePokedex).toBeDefined();
+  });
+
+  test('O botão deve conter o texto Próximo pokémon', () => {
+    renderWithRouter(<App />);
+
+    const titleBtn = screen.getByRole('button',
+      { name: /Próximo pokémon/i });
+    expect(titleBtn).toBeDefined();
+  });
+
+  test('O próximo pokémon é mostrado, ao clicar no botão Próximo pokémon ', () => {
+    renderWithRouter(<App />);
+
+    const nextPokemon = screen.getAllByTestId('pokemon-name');
+
+    const botaoNextPokemon = screen.getByRole('button', { name: /Próximo pokémon/i });
+    userEvent.click(botaoNextPokemon);
+
+    const pokemonOne = screen.getByText(/charmander/i);
+    expect(pokemonOne).toBeInTheDocument();
+    expect(nextPokemon).toHaveLength(1);
+
+    userEvent.click(botaoNextPokemon);
+    const pokemonTwo = screen.getByText(/caterpie/i);
+    expect(pokemonTwo).toBeInTheDocument();
+    expect(nextPokemon).toHaveLength(1);
+  });
+
+  test('se é mostrado apenas um pokémon por vez', () => {
+    renderWithRouter(<App />);
+
+    const presentPokemon = screen.getAllByText(/pikachu/i);
+
+    expect(presentPokemon.length).toBe(1);
+  });
+
+  test('os botões de filtragem', () => {
+    renderWithRouter(<App />);
+
+    const numberButtons = 7;
+    const buttonFilter = screen.getAllByTestId(/pokemon-type-button/i);
+    expect(buttonFilter).toHaveLength(numberButtons);
+
+    const typeEletric = screen.getAllByText(/electric/i);
+    expect(typeEletric).toBeDefined();
+
+    const typePsychic = screen.getAllByText(/psychic/i);
+    expect(typePsychic).toBeDefined();
+  });
+
+  test('se tem um botão para resetar o filtro', () => {
+    renderWithRouter(<App />);
+
+    const buttonReset = screen.getByText(/all/i);
+    userEvent.click(buttonReset);
+    expect(buttonReset).toBeDefined();
+    expect(buttonReset.type).toBe('button');
+  });
+});
--- src/tests/Pokemon.test.js
+++ src/tests/Pokemon.test.js
@@ -1,1 +1,47 @@
-test('', () => {});
+import React from 'react';
+import { screen } from '@testing-library/react';
+import userEvent from '@testing-library/user-event';
+import renderWithRouter from '../renderWithRouter';
+import App from '../App';
+import pokemons from '../data';
+import { FavoritePokemons } from '../pages';
+
+describe('', () => {
+  test('se é renderizado um card com as informações de determinado pokémon', () => {
+    renderWithRouter(<App />);
+
+    const pikachu = pokemons[0];
+
+    const name = screen.getByTestId('pokemon-name');
+    expect(name).toBeInTheDocument(pikachu.name);
+
+    const type = screen.getAllByTestId(/pokemon-type/i);
+    expect(type[0].textContent).toBe('Electric');
+
+    const averageWeight = screen.getByTestId('pokemon-weight');
+    expect(averageWeight).toBeInTheDocument(`Average weight:${pikachu.averageWeight.value}
+     ${pikachu.averageWeight.measurementUnit}`);
+
+    const image = screen.getByAltText(/pikachu sprite/i);
+    expect(image).toHaveAttribute('src', 'https://cdn2.bulbagarden.net/upload/b/b2/Spr_5b_025_m.png');
+  });
+
+  test('a página de detalhes', () => {
+    const { history } = renderWithRouter(<App />);
+
+    const details = screen.getByRole('link', { name: 'More details' });
+    userEvent.click(details);
+
+    const { pathname } = history.location;
+    expect(pathname).toBe('/pokemons/25');
+  });
+
+  test('se o pokemon for favoritado há um ícone de estrela', () => {
+    const pikachu = [pokemons[0]];
+
+    renderWithRouter(<FavoritePokemons pokemons={ pikachu } />);
+
+    const favoritePokemon = screen.getByAltText(/pikachu is marked as favorite/i);
+    expect(favoritePokemon).toHaveAttribute('src', '/star-icon.svg');
+  });
+});
